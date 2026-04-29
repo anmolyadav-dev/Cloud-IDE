b40256cf438683d9b9380e502e12eac13f1d777f
Anmol yadav <work.anmolyadav@gmail.com>
2026-04-29 12:22:29 +0530

    Move server files and add frontend client

---
+++ client/src/components/terminal.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useRef } from 'react'
+import { Terminal as XTerminal } from '@xterm/xterm'
+import '@xterm/xterm/css/xterm.css'
+import socket from '../socket'
+const Terminal = () => {
+    const terminalRef = useRef(null)
+    const isRendered = useRef(false)
+
+    useEffect(() => {
+        if (isRendered.current) return
+        isRendered.current = true
+
+        const term = new XTerminal({
+            rows: 20
+        })
+        term.open(terminalRef.current)
+        term.onData((data) => {
+            socket.emit("terminal:write", data)
+        })
+        socket.on("terminal:data", (data) => {
+            term.write(data)
+        })
+    }, [])
+    return (
+        <div ref={terminalRef} id='terminal'>
+
+        </div>
+    )
+}
+
+export default Terminal
--- server/index.js
+++ server/index.js
@@ -27,7 +27,7 @@ ptyProcess.onData(data => {
 
 io.on("connection", (socket) => {
     console.log(`socket connected`, socket.id)
-    socket.on('termnal:write', (data) => {
+    socket.on('terminal:write', (data) => {
         ptyProcess.write(data)
     })
 })
